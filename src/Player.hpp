2f88120b98b270742bf4ea709be1ad2110e5da8d
adorno-dev <adorno.dev@gmail.com>
2026-09-23 12:54:35 -0300

    Player Class & Component System

---
+++ src/Player.hpp
@@ -0,0 +1,20 @@
+#pragma once
+
+#include "raylib.h"
+#include "Transform2D.hpp"
+#include "Sprite.hpp"
+#include "Movement.hpp"
+
+class Player
+{
+public:
+    Player(Texture2D* texture);
+    void Draw() const;
+    void Update(Vector2 moveDir, float delta);
+    Vector2 GetPosition() const;
+    void SetPosition(Vector2 position);
+private:
+    Transform2D _transform;
+    Sprite _sprite;
+    Movement _movement;
+};
--- src/main.cpp
+++ src/main.cpp
@@ -1,10 +1,7 @@
 #include <algorithm>
 #include "raylib.h"
-
-#define EXIT_SUCCESS 0
-
-constexpr int BASE_W = 1280;
-constexpr int BASE_H = 720;
+#include "GameConfig.hpp"
+#include "Player.hpp"
 
 int main()
 {
@@ -21,10 +18,10 @@ int main()
     float mapH;
 
     float halfW;
-    float halfH = BASE_H * 0.5f;
+    float halfH = GameConfig::BASE_H * 0.5f;
 
     SetConfigFlags(FLAG_WINDOW_RESIZABLE);
-    InitWindow(BASE_W, BASE_H, "Swarm");
+    InitWindow(GameConfig::BASE_W, GameConfig::BASE_H, "Swarm");
     SetTargetFPS(60);
     
     // Vector2 pos = GetMonitorPosition(0);
@@ -35,56 +32,68 @@ int main()
 
     Texture2D background = LoadTexture("Floor.png");
     Texture2D walls = LoadTexture("Walls.png");
-    // Texture2D playerTexture = LoadTexture("survivor-idle_shotgun_0.png");
+    Texture2D playerTexture = LoadTexture("survivor-idle_shotgun_0.png");
 
-    RenderTexture2D canvas = LoadRenderTexture(BASE_W, BASE_H);
+    RenderTexture2D canvas = LoadRenderTexture(GameConfig::BASE_W, GameConfig::BASE_H);
     SetTextureFilter(canvas.texture, TEXTURE_FILTER_BILINEAR);
 
     mapW = (float)background.width;
     mapH = (float)background.height;
 
-    halfW = BASE_W * 0.5f;
-    halfH = BASE_H * 0.5f;
+    halfW = GameConfig::BASE_W * 0.5f;
+    halfH = GameConfig::BASE_H * 0.5f;
+
+    Player player(&playerTexture);
+    player.SetPosition({mapW * 0.5f, mapH * 0.5f});
 
     Camera2D camera = {};
     camera.zoom = 1.0f;
-    camera.target = {400.0f, 400.0f};
+    camera.target = player.GetPosition();
     camera.offset = { halfW, halfH };
-
-    const char* cameraLabelText;
 
     while (!WindowShouldClose())
     {
+        if (IsKeyPressed(KEY_F1))
+            Sprite::showDebug = !Sprite::showDebug;
+
         dt = GetFrameTime();
+
         moveX = (IsKeyDown(KEY_D) ? 1.0f : 0.0f) - (IsKeyDown(KEY_A) ? 1.0f : 0.0f);
         moveY = (IsKeyDown(KEY_S) ? 1.0f : 0.0f) - (IsKeyDown(KEY_W) ? 1.0f : 0.0f);
-        camera.target.x += dt * 300.0f * moveX;
-        camera.target.y += dt * 300.0f * moveY;
+
+        player.Update({moveX, moveY}, dt);
+
+        camera.target = player.GetPosition();
+
         camera.target.x = std::clamp(camera.target.x, halfW, mapW - halfW);
         camera.target.y = std::clamp(camera.target.y, halfH, mapH - halfH);
-        cameraLabelText = TextFormat("Camera: %.0f, %.0f", camera.target.x, camera.target.y);
-
+        
         BeginTextureMode(canvas);
             ClearBackground(BLACK);
             BeginMode2D(camera);
                 DrawTexture(background, 0, 0, WHITE);
-                // DrawTextureEx(playerTexture, {400.0f, 400.0f}, 0.0f, 0.3f, WHITE);
+                player.Draw();
                 DrawTexture(walls, 0, 0, WHITE);
             EndMode2D();
-            DrawRectangle(0, BASE_H - 32, BASE_W, 32, ColorAlpha(DARKBLUE, 0.6f));
-            DrawText(cameraLabelText, 12, BASE_H - 24, 20, LIME);
+
+            DrawRectangle(0, GameConfig::BASE_H - 32, GameConfig::BASE_W, 32, ColorAlpha(DARKBLUE, 0.6f));
+
+            DrawText(TextFormat("Player: %.0f,%.0f", player.GetPosition().x, player.GetPosition().y), 12, GameConfig::BASE_H - 24, 20, LIME);
+            DrawText(TextFormat("Camera: %.0f,%.0f", camera.target.x, camera.target.y), 256, GameConfig::BASE_H - 24, 20, LIME);
+            DrawText(TextFormat("Map: %.0f,%.0f", mapW, mapH), 512, GameConfig::BASE_H - 24, 20, LIME);
+
         EndTextureMode();
 
         scale = std::min(
-            (float)GetScreenWidth() / BASE_W,
-            (float)GetScreenHeight() / BASE_H
+            (float)GetScreenWidth() / GameConfig::BASE_W,
+            (float)GetScreenHeight() / GameConfig::BASE_H
         );
 
-        offsetX = (GetScreenWidth() - BASE_W * scale) * 0.5f;
-        offsetY = (GetScreenHeight() - BASE_H * scale) * 0.5f;
+        offsetX = (GetScreenWidth() - GameConfig::BASE_W * scale) * 0.5f;
+        offsetY = (GetScreenHeight() - GameConfig::BASE_H * scale) * 0.5f;
 
-        Rectangle src = { 0, 0, (float)BASE_W, -(float)BASE_H };
-        Rectangle dst = { offsetX, offsetY, BASE_W * scale, BASE_H * scale };
+        Rectangle src = { 0, 0, (float)GameConfig::BASE_W, -(float)GameConfig::BASE_H };
+        Rectangle dst = { offsetX, offsetY, GameConfig::BASE_W * scale, GameConfig::BASE_H * scale };
 
         BeginDrawing();
             ClearBackground(BLACK);
@@ -94,7 +103,7 @@ int main()
 
     UnloadTexture(background);
     UnloadTexture(walls);
-    // UnloadTexture(playerTexture);
+    UnloadTexture(playerTexture);
     UnloadRenderTexture(canvas);
     CloseWindow();
 
